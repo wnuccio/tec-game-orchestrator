b1ebd86f8662727cc24b2f5cbf0db741f84e7503
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2025-04-18 11:38:21 +0200

    Move context to field

--- src/test/java/tests/SubscribeUserTest.java
+++ src/test/java/tests/SubscribeUserTest.java
@@ -2,15 +2,22 @@ package tests;
 
 import collections.TestContext;
 import entities.SubscribedUserResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SubscribeUserTest {
+
+    private TestContext context;
+
+    @BeforeEach
+    void setUp() {
+        context = new TestContext();
+    }
+
     @Test
     void user_subscribes_on_website() {
-        TestContext context = new TestContext();
-
         SubscribedUserResult subscribedUserResult = context.subscribedUserEntity().get();
 
         assertNotNull(subscribedUserResult);
